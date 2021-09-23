414a299376cf4209aecf729a48a71f108bbbe5be
actpohabtNS <actpohabt.ns@gmail.com>
2021-09-23 23:12:16 +0300

    [A] Barbershop struct, barber func

---
+++ courses/golang/ex06-barbershop/barbershop.go
@@ -0,0 +1,13 @@
+package main
+
+type Barbershop struct {
+	seats           int
+	customersPerDay int
+	customersServed int
+	customerDelay   int
+	barberDelay     int
+}
+
+func barber(barbershop *Barbershop, barbershopClosing chan bool) {
+
+}
