505803e97dd84dc814eeffbc6e60437eb7025821
actpohabtNS <actpohabt.ns@gmail.com>
2021-09-26 00:18:32 +0300

    [Fea] shutting down with message

--- courses/golang/golang-api/main.go
+++ courses/golang/golang-api/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"context"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func getCakeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("cake"))
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("cake"))
+	if err != nil {
+		return
+	}
 }
+
 func main() {
-	http.HandleFunc("/cake", getCakeHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/cake", getCakeHandler).Methods(http.MethodGet)
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	go func() {
+		<-interrupt
+		ctx, cancel := context.WithTimeout(context.Background(),
+			5*time.Second)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			return
+		}
+	}()
 	log.Println("Server started, hit Ctrl+C to stop")
-	err := http.ListenAndServe(":8080", nil)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Println("Server exited with error:", err)
 	}
